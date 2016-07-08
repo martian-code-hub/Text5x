247f6fca6043121a9f8009b48fb153e66e2174ca
0pipi0 <yangpei2548914@sina.com>
2016-07-08 12:19:19 +0800

    commit

---
+++ app/src/main/java/com/example/administrator/view/AnimationActivity.java
@@ -0,0 +1,109 @@
+package com.example.administrator.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.example.administrator.fragment.DrawerContentFragment;
+import com.example.administrator.fragment.DrawerLeftFragment;
+import com.example.administrator.text5x.R;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/6.
+ */
+public class AnimationActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+    private String mTitle;
+
+    private Toolbar toolbar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        int flag = getIntent().getExtras().getInt("flag");
+        mTitle = getIntent().getExtras().getString("title");
+        switch (flag) {
+            case 0:
+                getWindow().setEnterTransition(new Explode());
+                break;
+            case 1:
+                getWindow().setEnterTransition(new Slide());
+                break;
+            case 2:
+                getWindow().setEnterTransition(new Fade());
+                break;
+        }
+
+        setContentView(R.layout.activity_animation);
+        iniView();
+    }
+
+
+
+    private void iniView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_toolbar);
+        toolbar.setBackgroundColor(this.getResources().getColor(R.color.colorPrimaryDark));
+//        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setTitle(mTitle);
+//        toolbar.setSubtitle("toolbar");
+//        toolbar.inflateMenu(R.menu.menu_main);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
