88add086de962ba5ce0d2ee7b938cd59c4ce0f94
0pipi0 <yangpei2548914@sina.com>
2016-07-08 18:17:26 +0800

    commit

--- app/src/main/java/com/example/administrator/view/NotifitionActivity.java
+++ app/src/main/java/com/example/administrator/view/NotifitionActivity.java
@@ -18,6 +18,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.example.administrator.text5x.R;
@@ -84,17 +86,31 @@ public class NotifitionActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createNotifition();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                createNotifition();
+//            }
+//        });
     }
 
-    private void createNotifition(){
+    public void click(View view){
+        switch (view.getId()){
+            case R.id.activity_notifition_basic:
+                createBasicNotifition();
+                break;
+            case R.id.activity_notifition_collapsed:
+                createCollapsedNotifition();
+                break;
+            case R.id.activity_notifition_headsup:
+                createHeadsupNotifition();
+                break;
+        }
+    }
+
+
+    private void createBasicNotifition(){
         Notification.Builder builder = new Notification.Builder(this);
 
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
@@ -113,6 +129,58 @@ public class NotifitionActivity extends AppCompatActivity {
         notificationManager.notify(0,builder.build());
     }
 
+
+    private void createCollapsedNotifition(){
+        Notification.Builder builder = new Notification.Builder(this);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sina.com"));
+        PendingIntent pendingIntent =  PendingIntent.getActivity(this,0,intent,0);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentTitle("My Notifition");
+//        builder.setContentText("this is content");
+        builder.setSubText("this is sub content");
+
+        RemoteViews expendView = new RemoteViews(getPackageName(),R.layout.notification_bigview);
+        expendView.setTextViewText(R.id.notification_bigview_tv, "this is bigview content");
+        RemoteViews remoteView = new RemoteViews(getPackageName(),R.layout.notification_remoteview);
+        remoteView.setTextViewText(R.id.notification_remoteview_tv, "this is remoteview content");
+
+        Notification notification =   builder.build();
+        notification.bigContentView = expendView;
+        notification.contentView = remoteView;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1,notification);
+    }
+
+
+    private void createHeadsupNotifition(){
+        Notification.Builder builder = new Notification.Builder(this);
+
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setClass(this,MainActivity.class);
+        PendingIntent pendingIntent =  PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setPriority(Notification.PRIORITY_DEFAULT);
+        builder.setCategory(Notification.CATEGORY_TRANSPORT);
+//        builder.setContentIntent(pendingIntent);
+//        builder.setAutoCancel(true);
+//        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentTitle("My Notifition");
+        builder.setContentText("this is content");
+//        builder.setSubText("this is sub content");
+
+        builder.setFullScreenIntent(pendingIntent,true);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(2,builder.build());
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
